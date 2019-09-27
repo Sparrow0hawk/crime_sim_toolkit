e5741271671390d5b072c56f113d66df4eabd5d9
sparrow0hawk <alexjcoleman@gmail.com>
2019-09-27 13:40:45 +0100

    removed whitespace in test_all

--- crime_sim_toolkit/tests/test_all.py
+++ crime_sim_toolkit/tests/test_all.py
@@ -480,7 +480,6 @@ class Test(unittest.TestCase):
 
         self.assertEqual(self.poi_data.shape[0], 14 * 6)
 
-<<<<<<< HEAD
     def test_moving_window_week(self):
 
         self.week = 4
@@ -520,8 +519,6 @@ class Test(unittest.TestCase):
                                       "2016-12-30"]
                                       )
 
-=======
->>>>>>> 8122df2d303d161d34b88de91fa95d158de8d556
     def test_sample_perturb(self):
         """
         Test that adding zero function works
@@ -537,9 +534,6 @@ class Test(unittest.TestCase):
 
         self.assertEqual(self.testneg.loc[4,'Counts'], 10)
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 8122df2d303d161d34b88de91fa95d158de8d556
 if __name__ == "__main__":
     unittest.main(verbosity=2)
